11ff4f9a44c4548fa02ee83747c34ee78f417c77
Guilherme de Moura Oliveira <guilherme@microbit.com.br>
2025-03-31 08:56:26 -0300

    v1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://vite-project-api.onrender.com'
+    apiUrl: 'https://vite-project-api.onrender.com',
+    version: '1.0.0'
 };
